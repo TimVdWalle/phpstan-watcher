af3e8557d3d51eb30fe6707ec355f2620d342468
timvandewalle <tim@vandewalle.mobi>
2022-10-02 18:23:08 +0200

    refactored + code cleanup + code formatting

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ let isBusy = false;
 let pwd = null;
 
 var clc = require("cli-color");
+const {exec} = require("child_process");
+const watch = require("node-watch");
 
 function main() {
     // initialize + welcome message
@@ -16,7 +18,7 @@ function main() {
     // startWwatching();
 }
 
-function init(){
+function init() {
     const {cwd} = require('node:process');
     pwd = cwd();
 
@@ -24,7 +26,7 @@ function init(){
     console.log('');
 }
 
-function startWwatching(){
+function startWwatching() {
     var watch = require('node-watch');
 
     watch('.', {filter: /\.php$/, recursive: true}, function (evt, name) {
@@ -36,12 +38,12 @@ function startWwatching(){
     });
 }
 
-async function analyse() {
+function analyse() {
     isBusy = true;
     const {exec} = require("child_process");
     let result = null;
 
-    await exec('composer analyse-json', (err, output) => {
+    exec('composer analyse-json', (err, output) => {
         // once the command has completed, the callback function is called
         if (err) {
             // console.error("could not execute command: ", err)
@@ -55,12 +57,9 @@ function showResults(results) {
     isBusy = false;
     results = JSON.parse(results);
     showFileResults(results.files);
-    // results = JSON.parse(results);
-    // console.log(results);
 }
 
 function showFileResults(results) {
-
     for (var file in results) {
         if (results.hasOwnProperty(file)) {
             const displayFile = file.replace(pwd, "");
@@ -68,53 +67,23 @@ function showFileResults(results) {
             const errors = obj['errors'];
             const messages = obj['messages'];
 
-            printHeaderLine();
-            let str = ' ' + 'Line   ' + displayFile;
-            const width = getOutputWidth() - str.length;
-            for (i = 0; i < width; i++) {
-                str = str + ' ';
-            }
-
-            console.log(clc.white.bgBlue(str));
-            printHeaderLine();
-
+            showHeader(displayFile);
             showFileDetailResults(messages);
-
-
-            // console.log(file);
-            // console.log(file + ': ' + errors);
         }
     }
 }
 
-function showFileDetailResults(results) {
-    // console.log(results[0]);
-
-    for (var warning in results) {
-        const obj = results[warning];
-
-        const line = obj.line;
-        const message = obj.message;
-        const ignorable = obj.ignorable;
-
-        printWrappedLine(line, message);
-        // console.log(line + ' ' + message + ' ' + ignorable);
+function showHeader(displayFile) {
+    let str = ' ' + 'Line   ' + displayFile;
+    const width = getOutputWidth() - str.length;
+    for (i = 0; i < width; i++) {
+        str = str + ' ';
     }
-}
 
-function printWrappedLine(str1, str2){
-    var cliFormat = require('cli-format');
-
-    var col1 = str1.toString();
-    var col2 = {
-        content: str2,
-        width: 80,
-    };
-
-    var config = { width: 89, paddingMiddle: ' ' };
-
-    var result = cliFormat.columns.wrap([col1, col2], config);
-    console.log(result);
+    console.log();
+    printHeaderLine();
+    console.log(clc.white.bgRed(str));
+    printHeaderLine();
 }
 
 function printHeaderLine() {
@@ -124,13 +93,39 @@ function printHeaderLine() {
         str = str + '-';
     }
 
-    console.log(clc.white.bgBlue(str + ' '));
+    console.log(clc.white.bgRed(str + ' '));
+}
+
+function showFileDetailResults(results) {
+    for (var warning in results) {
+        const obj = results[warning];
+
+        const line = obj.line;
+        const message = obj.message;
+        const ignorable = obj.ignorable;
+
+        printWrappedLine(line, message);
+    }
+}
+
+function printWrappedLine(str1, str2) {
+    var cliFormat = require('cli-format');
+
+    var col1 = ' ' + str1.toString();
+    var col2 = {
+        content: str2,
+        width: getOutputWidth() - 9,
+    };
+
+    var config = {width: getOutputWidth(), paddingMiddle: ' '};
+
+    var result = cliFormat.columns.wrap([col1, col2], config);
+    console.log(result);
 }
 
 function getOutputWidth() {
     return process.stdout.columns;
 }
-
 
 if (require.main === module) {
     main();
